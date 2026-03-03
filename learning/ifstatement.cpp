d40519e017b5951bf9a4e1502966a5354fb315cb
Ammarali-byte <ummaraliummar75@gmail.com>
2026-03-04 01:21:41 +0500

    solved

---
+++ learning/ifstatement.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main (){
+    string name ;
+    cout<<"Enter your name : ";
+    cin>>name;
+    if (name=="Ammar"){
+        cout<<"welcome  "<<name<<endl;
+    }
+    cout<<"Program ends ";
+}
