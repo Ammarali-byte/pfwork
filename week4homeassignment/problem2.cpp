30af58b2ff412fc0f84119208e1e9f09fdf7460a
Ammarali-byte <ummaraliummar75@gmail.com>
2026-03-07 02:38:05 +0500

    solved

---
+++ week4homeassignment/problem2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std ;
+int main (){
+   int speed ;
+   cout<<"Enter car's speed : ";
+   cin >> speed ;
+   if ( speed > 100){
+    cout<< " Halt...... YOU WILL BE CHALLANED ";
+   }
+   else {
+    cout<< " Perfact ! You are going good ";
+   }
+}
