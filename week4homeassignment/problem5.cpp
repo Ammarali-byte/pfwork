cf6f254284de4024a44228cccb0c0e6ce6cf8d4b
Ammarali-byte <ummaraliummar75@gmail.com>
2026-03-11 14:50:47 +0500

    save

--- week4homeassignment/problem5.cpp
+++ week4homeassignment/problem5.cpp
@@ -1,10 +1,28 @@
 #include<iostream>
-using namespace std;
-int main (){
-    int holidays ,norms ;
-    cout<<" Enter tne number of holidays : ";
-    cin>> holidays;
-     
+using namespace std ;
+int main () {
+    int workingdays , timeforgames ,holidays ;
+    
+    cout<< " Enter the numberof holidays : ";
+    cin>>holidays;
+    workingdays = 365 - holidays ;
+    timeforgames = ( holidays * 127 ) + ( workingdays * 63 ); 
+    int normalnorms = 30000;
+    int norms = timeforgames - normalnorms ;
+    int hours = norms / 60 ;
+    int minutes = norms % 60 ;
+    
+    
 
+    if ( timeforgames <= normalnorms ){
+        cout <<"tom sleeps well \n";
+        cout <<hours<<" hours "<<minutes<< " minutes less for play ";
+    }
+    else{
+        cout<<"Tom will run away \n";
+        cout<<hours << " hours "<<minutes<< " minutes for play ";
+    }
+    
+    
 }
  
