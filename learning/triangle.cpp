74ffdfd7a32c4a9edb14fb87a32d4d59a5d55422
Ammarali-byte <ummaraliummar75@gmail.com>
2026-03-11 14:52:32 +0500

    save

--- learning/triangle.cpp
+++ learning/triangle.cpp
@@ -1,11 +1,42 @@
 #include<iostream>
 using namespace std;
-int main(){
-    int a=1234;
-    int b,c,d;
-    a=a%10;
-    b=a%10;
-    cout<<b;
+int main (){
+        
+    int totalDaysInYear = 365;
+    int annualNorm = 30000;
+    int playTimeWorkingDay = 63;
+    int playTimeHoliday = 127;
 
+    int holidays ;
+    cout << "Enter number of holidays: ";
+    cin >> holidays;
+    
+    int workingDays = totalDaysInYear - holidays;
 
-   }
+    
+    int totalPlayTime = (workingDays * playTimeWorkingDay) + (holidays * playTimeHoliday);
+
+   
+    int difference = annualNorm - totalPlayTime;
+
+    if (totalPlayTime <= annualNorm) {
+        
+        cout << "Tom sleeps well" << endl;
+        
+        int hours = difference / 60;
+        int minutes = difference % 60;
+        cout << hours << " hours and " << minutes << " minutes less for play" << endl;
+    } 
+    else {
+        
+        cout << "Tom will run away" << endl;
+        
+        
+        int overLimit = totalPlayTime - annualNorm;
+        int hours = overLimit / 60;
+        int minutes = overLimit % 60;
+        cout << hours << " hours and " << minutes << " minutes excess for play" << endl;
+    }
+
+    return 0;
+}
