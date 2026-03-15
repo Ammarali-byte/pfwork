9b2f71e655624c4721be903fe36515ed53b40545
Ammarali-byte <ummaraliummar75@gmail.com>
2026-03-15 15:34:16 +0500

    save

--- week5/hometasks/task16.cpp
+++ week5/hometasks/task16.cpp
@@ -1,8 +1,8 @@
 #include<iostream>
 using namespace std;
  main ( ){
-    for ( int i=1 ; i <= 3 ;i++){
-    for ( int j = 1 ; j <= 3 ; j++){
+    for ( int i=5 ; i >=1 ;i--){
+    for ( int j = 5 ; j >= i ; j--){
     cout<<j<<" ";
     }
     cout<<endl;
