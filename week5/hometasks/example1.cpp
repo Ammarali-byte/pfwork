e7e2f09fe45cb93b173cac51b8db9b214a31cbd6
Ammarali-byte <ummaraliummar75@gmail.com>
2026-03-16 19:34:32 +0500

    save

---
+++ week5/hometasks/example1.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+main ( ){
+    while(true){
+        int choice;
+        cout<<"\n=====MENU=====\n";
+        cout<<"1.Say Hello \n";
+        cout<<"2.Say Goodbye \n";
+        cout<<"3. Exit  \n";
+        cout<<"Enter youir choice :";
+        cin>>choice;
+
+        if (choice == 1 ){
+            cout<<"Hello ! Welcome to the program .\n";
+        }
+        else if ( choice == 2 ){
+            cout<<"Goodbye ! Have a nice day .\n";
+        }
+        else if ( choice == 3 ){
+            cout<<" Program ended .\n";
+            break;
+        }
+        else{
+            cout <<"Invalid choice . ";
+        }
+    }
+    return 0;
+}
