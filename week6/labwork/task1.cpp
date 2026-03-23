e3ac040e332d6112adc8126212577fec952d960c
Ammarali-byte <ummaraliummar75@gmail.com>
2026-03-23 20:21:41 +0500

    save

--- week6/labwork/task1.cpp
+++ week6/labwork/task1.cpp
@@ -1,5 +1,11 @@
 #include<iostream>
 using namespace std ;
 main ( ){
-    cout<<" ";
+    int number[5] = {15,12,13 ,19 , 10 };
+    cout << " The first element at location no [0] is "<< number[0]<<endl;
+    cout << " The second element at location no [1] is "<< number[1]<<endl;
+    cout << " The third element at location no [2] is "<< number[2]<<endl;
+    cout << " The fourth element at location no [3] is "<< number[3]<<endl;
+    cout << " The fifth element at location no [4] is "<< number[4]<<endl;
+    
 }
