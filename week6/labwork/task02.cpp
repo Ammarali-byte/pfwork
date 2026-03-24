ace917a04b33a8944fed1d738d46674b2524052d
Ammarali-byte <ummaraliummar75@gmail.com>
2026-03-24 21:46:57 +0500

    save

---
+++ week6/labwork/task02.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+main ( ){
+    int number [5];
+    cout << "Enter first number ";
+    cin>>number[0];
+    cout << "Enter second  number ";
+    cin>>number[1];
+    cout << "Enter third  number ";
+    cin>>number[2];
+    cout << "Enter fourth number ";
+    cin>>number[3];
+    cout << "Enter fifth number ";
+    cin>>number[4];
+    cout<<"The second element in arary is : "<< number[2]<<endl<<"The fourth element in arary is :  "<<number[4];
+}
