845791d7cb74212ab29210d5ebbede4d56ce088a
Ammarali-byte <ummaraliummar75@gmail.com>
2026-03-28 16:33:28 +0500

    save

--- week6/hometasks/task1.cpp
+++ week6/hometasks/task1.cpp
@@ -1,5 +1,19 @@
 #include<iostream>
 using namespace std ;
 main ( ){
-    
+    int n;
+    cout<<"Enter number of elements :"<<endl;
+    cin>>n;
+    int num[n];
+
+    cout<<"Enter "<<n<<" numbers ";
+    for ( int i = 0 ; i < n ; i++ ){
+        cin>>num[i];
+
+    }
+    int sum = 0 ;
+    for ( int i = 0 ; i < n ; i++ ){
+        sum = sum + num[i];
+    }
+    cout<<"Sum of all elements is : "<<sum;
 }
