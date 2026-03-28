2d6bf49614fdc9014efc7db5e044765c3d396b4a
Ammarali-byte <ummaraliummar75@gmail.com>
2026-03-28 15:14:59 +0500

    save

---
+++ week6/labwork/something.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+main ( ){
+    string sentence;
+    cout<<"Enter the sentence you want to enter : ";
+    getline ( cin , sentence );
+    cout<<"Something "<<sentence;
+}
