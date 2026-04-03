94cd0e705b15fe923a8e117aa8e87475ad0f7d2e
Ammarali-byte <ummaraliummar75@gmail.com>
2026-04-04 00:13:12 +0500

    save

---
+++ midsprep/2nd.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+main ( ){
+    int num;
+    cout<<"Enter the number : ";
+    cin>>num;
+    int sum= 0 ;
+    while (num >= 0 ){
+        sum = sum + num;
+        cout << "Enter the number : ";
+        cin >> num;
+    }
+    cout<<"Sum is : "<<sum;
+}
