51eeed81afeb8aa5e1d7b645eaea6d120e16bb92
Ammarali-byte <ummaraliummar75@gmail.com>
2026-04-04 00:48:19 +0500

    save

--- midsprep/ist.cpp
+++ midsprep/ist.cpp
@@ -1,12 +1,17 @@
 #include<iostream>
 using namespace std;
 main( ){
-    char choice ;
-    cout<<"ENter your choice : ";
-    cin>>choice;
-    while (choice != 'n' && choice != 'N'){
-        cout<<"OURS mids are starting from tuesday \n";
-        cout << "ENter your choice : \n";
-        cin >> choice;
+    // char choice ;
+    // cout<<"ENter your choice : ";
+    // cin>>choice;
+    // while (choice != 'n' && choice != 'N'){
+    //     cout<<"OURS mids are starting from tuesday \n";
+    //     cout << "ENter your choice : \n";
+    //     cin >> choice;
+    //}
+    int sum = 0 ;
+    for( int i = 1 ; i <= 5 ; i++ ){
+        sum = sum + i ;
     }
+    cout <<"sum is : "<<sum;
 }
