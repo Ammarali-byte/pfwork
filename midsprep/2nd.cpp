2151ac57d7d0c555e789ad851c4c4aef44b221f1
Ammarali-byte <ummaraliummar75@gmail.com>
2026-04-04 01:52:19 +0500

    save

--- midsprep/2nd.cpp
+++ midsprep/2nd.cpp
@@ -1,16 +1,18 @@
 #include<iostream>
 using namespace std;
 main ( ){
-    int num;
-    cout<<"Enter the number : ";
-    cin>>num;
-    int sum= 0 ;
-    while (num >= 0 ){
-        sum = sum + num;
-        cout << "Enter the number : ";
-        cin >> num;
+   int digit , frequency = 0 ,n; 
+   cout<<"Enter the digit : ";
+   cin>>digit;
+   cout<<"enter the digit whose frequency you want to  calculate : ";
+   cin>>n;
+   for(int i = digit ; i > 0 ; i= i / 10){
+    int rem = i % 10;
+    if (rem == n ){
+        frequency = frequency + 1 ;
     }
-    cout<<"Sum is : "<<sum;
-    
+    digit = digit / 10 ;
+   }
+   cout<<"Frequency is "<<frequency;
 
 }
