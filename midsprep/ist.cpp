083ab13bccac9ca876d87bf37eb1ac63ef607147
Ammarali-byte <ummaraliummar75@gmail.com>
2026-04-04 01:03:50 +0500

    save

--- midsprep/ist.cpp
+++ midsprep/ist.cpp
@@ -1,17 +1,15 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-main( ){
-    // char choice ;
-    // cout<<"ENter your choice : ";
-    // cin>>choice;
-    // while (choice != 'n' && choice != 'N'){
-    //     cout<<"OURS mids are starting from tuesday \n";
-    //     cout << "ENter your choice : \n";
-    //     cin >> choice;
-    //}
-    int sum = 0 ;
-    for( int i = 1 ; i <= 5 ; i++ ){
-        sum = sum + i ;
+main()
+{
+    int num, count = 0;
+    cout << "ENter the number : ";
+
+    cin >> num;
+    for (int i = num; i > 0; i = i / 10)
+    {
+        count = count + 1;
+        num = num % 10;
     }
-    cout <<"sum is : "<<sum;
+    cout<<"total digits  = "<<count;
 }
