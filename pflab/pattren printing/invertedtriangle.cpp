3ff327430f29a55f385d901f34414808767c86e1
Ammarali-byte <ummaraliummar75@gmail.com>
2026-04-05 00:41:22 +0500

    prep

--- pflab/pattren printing/invertedtriangle.cpp
+++ pflab/pattren printing/invertedtriangle.cpp
@@ -1,20 +1,16 @@
-#include<iostream>
-using namespace std ;
-main ( )
+#include <iostream>
+using namespace std;
+main()
 {
-    cout<<"enter the lsize of triangle  : ";
-    int n ; cin >> n;
-      for ( int i = 1 ; i <= n ; i++){
-        for ( int j = 1 ; j <=i ; j++ ){
-            cout << " ";
+    cout << "enter the size of triangle  : ";
+    int n;
+    cin >> n;
+    for (int i = n; i >= 1; i--)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << "* ";
         }
-        cout<<endl;
-        
-    }
-    for ( int i = 1 ; i <= n ; i++){
-        for ( int j = i ; j >=1 ; j++ ){
-            cout << "*";
-        }
-        cout<<endl;
+        cout << endl;
     }
 }
