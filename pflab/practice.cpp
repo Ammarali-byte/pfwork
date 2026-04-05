3d60c02783999004ac7b7afd586023ca0a01e876
Ammarali-byte <ummaraliummar75@gmail.com>
2026-04-05 14:39:54 +0500

    practice

--- pflab/practice.cpp
+++ pflab/practice.cpp
@@ -1,10 +1,16 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-main( ){
-    for (int i = 1 ; i <= 3 ; i++ ){
-        for (int j = 1 ; j <= 3 ; j++ ){
-            cout<<j<<" ";
-        }
-        cout<<endl;
+
+int main()
+{
+    int n = 5;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j < i; j++)
+            cout << " ";
+        for (int j = i; j <= n; j++)
+            cout << "*";
+        cout << endl;
     }
+    return 0;
 }
