af3e8baab8c93876ac80d6147c7170248f5d8e55
Ammarali-byte <ummaraliummar75@gmail.com>
2026-04-05 15:37:27 +0500

    Predicting output

--- pflab/practice.cpp
+++ pflab/practice.cpp
@@ -3,14 +3,9 @@ using namespace std;
 
 int main()
 {
-    int n = 5;
-    for (int i = 1; i <= n; i++)
-    {
-        for (int j = 1; j < i; j++)
-            cout << " ";
-        for (int j = i; j <= n; j++)
-            cout << "*";
-        cout << endl;
-    }
-    return 0;
+   int x, sum = 0;
+   for (x = 1 ; x== 50 ; ++x ){
+    sum+= x* x;
+   }
+   cout<<"Sum = "<<sum;
 }
