1b325b1abe424d64d60c9a987db7bbe83f9299f5
Ammarali-byte <ummaraliummar75@gmail.com>
2026-04-05 13:56:52 +0500

    Diamond printed successfully using nested loops

--- pflab/pattren printing/diamond.cpp
+++ pflab/pattren printing/diamond.cpp
@@ -5,11 +5,20 @@ main()
     cout << "enter the length or width of square : ";
     int n;
     cin >> n;
-    for (int i = n; i >= 1; i--)
+    for (int i = 1 ; i <= n ; i++  ){
+        for ( int j = 1 ; j <= n-i ; j++ ){
+            cout<<" ";
+        }
+        for (int j = 1 ; j <= i ; j++ ){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+    for (int i = n - 1; i >= 1; i--)
     {
         for (int j = 1; j <= n - i; j++)
         {
-            cout << "#";
+            cout << " ";
         }
         for (int j = 1; j <= i; j++)
         {
