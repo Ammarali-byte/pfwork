c02c46273cecd47095e4ec47dc5f4a3917bd299c
Ammarali-byte <ummaraliummar75@gmail.com>
2026-04-07 15:59:46 +0500

    practice

--- pflab/pattren printing/pyramidtrinanle.cpp
+++ pflab/pattren printing/pyramidtrinanle.cpp
@@ -5,17 +5,25 @@ main()
     cout << "enter the size of triangle  : ";
     int n;
     cin >> n;
-    // system("Color 07");
-    for (int i = 1; i <= n; i++)
-    {
-        for (int j = 1; j <= n - i; j++)
-        {
-            cout << " ";
-        }
-        for (int j = 1; j <= i; j++)
-        {
-            cout << "* ";
-        }
-        cout << endl;
+//     // system("Color 07");
+//     for (int i = 1; i <= n; i++)
+//     {
+//         for (int j = 1; j <= n - i; j++)
+//         {
+//             cout << " ";
+//         }
+//         for (int j = 1; j <= i; j++)
+//         {
+//             cout << "* ";
+//         }
+//         cout << endl;
+//     }
+// }
+
+for (int i = 1 ; i <= n ; i++  ){
+    for (int j = 1 ; j <= n - i ;  j++){
+        cout<<" ";
     }
+    cout<<endl;
+}
 }
