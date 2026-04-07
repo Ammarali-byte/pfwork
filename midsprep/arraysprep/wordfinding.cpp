06f2e94397ed0c4318d3d28f0077d13c9a629293
Ammarali-byte <ummaraliummar75@gmail.com>
2026-04-07 15:57:53 +0500

    mids preperation

---
+++ midsprep/arraysprep/wordfinding.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+main( ){
+    string name;
+    cout<<"Enter the word : ";
+    cin>>name;
+    char letter;
+    cout<<"Enter the alphabet you want to find : ";
+    cin>>letter;
+    bool found = false;
+    for(int i = 0 ; i < '\0' ; i++ ){
+        if (name[i] == letter){
+            bool found = true;
+        }else{
+            found = false;
+        }
+        
+    }
+    if (found = true ){
+        cout<<letter <<" is found in  "<<name<<endl;
+    }
+    else{
+        cout << letter << " is not  found in  " << name << endl;
+    }
+}
