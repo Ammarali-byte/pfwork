4787cccdcf1b7743f6d28fb1012fe00d9e6effbf
Ammarali-byte <ummaraliummar75@gmail.com>
2026-04-16 13:15:36 +0500

    save

--- project/SSRmanagementapp.cpp
+++ project/SSRmanagementapp.cpp
@@ -4,12 +4,16 @@
 using namespace std;
 main()
 {
+    string nameArr[10];
+    int dateArr[10];
+    double cnicArr[10];
     while (true)
     {
-        //main header
+
+        // main header
         system("color 06");
-        // system("cls");
-        cout << "     ###########        ####     ####    #######          \n";  
+        system("cls");
+        cout << "     ###########        ####     ####    #######          \n";
         cout << "  ###   ####    ###   ##    ## ##    ##  ##     ##                         \n";
         cout << " ###  ##    ##   ###  ##       ##        ##      ##             \n";
         cout << " ###  ##    ##   ###    ####     ####    ## #####                         \n";
@@ -29,32 +33,38 @@ main()
         cout << "Welcome to SERENITY SPRING RESORT \n";
         cout << "1-Management menu \n";
         cout << "2-Guest menu \n";
-        cout<<"3-Information about leepa valley \n";
-        cout<<"4- Exit \n";
+        cout << "3-Information about leepa valley \n";
+        cout << "4- Exit \n";
 
-        //taking choice from user
+        // taking choice from user
         string choice;
         cout << "Enter your choice ";
-        cin>>choice;
+        cin >> choice;
         if (choice == "1")
         {
-            // system("cls");   
+            system("cls");
             cout << "Mangement menu : \n";
+
+            
         }
         else if (choice == "2")
         {
             system("cls");
             cout << "GUEST MENU \n";
-            cout<<"Enter your name :";
+            cout << "\nEnter your name :";
             string name;
             getline(cin, name);
-            cout<<"Enter the date of your reservation : ";
-            
+            cout << "\n Enter the date of your reservation : ";
+            int date;
+            cin >> date;
         }
         else if (choice == "3")
         {
             system("cls");
             cout << "Information about Leepa valley kashmir \n";
+            cout << "Leepa valley is located in Azad Kashmir \n";
+
+            
         }
         else if (choice == "4")
         {
@@ -67,6 +77,8 @@ main()
             system("cls");
             cout << "Wrong option chosed \n";
         }
+        cout << "\n\n\nPress any key to continue \n";
+        getch();
     }
-    cout<<"Thanks for using this software ";
+    cout << "Thanks for using this software ";
 }
