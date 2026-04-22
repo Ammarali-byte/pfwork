45d603fcc2828cd70cbe98022c9a5c8fca265e01
Ammarali-byte <ummaraliummar75@gmail.com>
2026-04-23 02:53:16 +0500

    save

--- week10/hometasks/task2.cpp
+++ week10/hometasks/task2.cpp
@@ -1,2 +1,40 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
+
+float volume(float length, float width , float height);
+
+main()
+{
+    float length, width, height;
+    cout << "Enter length ,width, height (in meters )";
+    cin >> length;
+    cin >> width;
+    cin >> height;
+    string unit;
+    cout << "Enter output unit(millimeters ,centimeters ,meters, kilometers)";
+    cin >> unit;
+    float vol = volume(length, width, height);
+    if (unit == "millimeters")
+    {
+        vol = vol * 1000 *1000 * 1000;
+    }
+    else if (unit == "centimeters")
+    {
+        vol = vol * 100 *100*100;
+    }
+    else if (unit == "meters")
+    {
+        vol = vol ;
+    }
+    else if (unit == "kilometers")
+    {
+        vol = vol * 0.001 * 0.001 * 0.001;
+    }
+    cout<<"Volume = "<<vol<<" cubic "<<unit;
+}
+float volume(float length, float width, float height)
+{
+    float basearea = length * width;
+    float volume =( basearea* height)/3;
+    return volume;
+}
