4021182ab2f87ee11446b3a67e368161be522102
Ammarali-byte <ummaraliummar75@gmail.com>
2026-04-27 20:47:35 +0500

    practice

--- pflab/pattren printing/pyramidtrinanle.cpp
+++ pflab/pattren printing/pyramidtrinanle.cpp
@@ -5,25 +5,18 @@ main()
     cout << "enter the size of triangle  : ";
     int n;
     cin >> n;
-//     // system("Color 07");
-//     for (int i = 1; i <= n; i++)
-//     {
-//         for (int j = 1; j <= n - i; j++)
-//         {
-//             cout << " ";
-//         }
-//         for (int j = 1; j <= i; j++)
-//         {
-//             cout << "* ";
-//         }
-//         cout << endl;
-//     }
-// }
-
-for (int i = 1 ; i <= n ; i++  ){
-    for (int j = 1 ; j <= n - i ;  j++){
-        cout<<" ";
+    // system("Color 07");
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n - i; j++)
+        {
+            cout << " ";
+        }
+        for (int j = 1; j <= i; j++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
     }
-    cout<<endl;
 }
-}
+
--- pflab/pattren printing/tempCodeRunnerFile.cpp
+++ pflab/pattren printing/tempCodeRunnerFile.cpp
@@ -1,16 +1,3 @@
-#include<iostream>
-using namespace std;
-main ( ){
-    cout << "enter the length or width of square : ";
-    int n;
-    cin >> n;
-    for (int i = n ; i >= 1 ; i-- ){
-        for (int j = 1 ; j <= i; j++  ){
-            cout<<"#";
-        }
-        // for (int j = 1 ; j <= i ;j-- ){
-        //     cout<<"*";
+ // for (int j = 0 ; j <= i; j++){
+        //     cout<<"* ";
         // }
-        cout<<endl;
-    }
-}
