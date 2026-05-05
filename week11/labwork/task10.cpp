62fd8732db651f173b9e63c52fd5989710b9efa0
Ammarali-byte <ummaraliummar75@gmail.com>
2026-05-06 00:48:55 +0500

    week11

--- week11/labwork/task10.cpp
+++ week11/labwork/task10.cpp
@@ -1,16 +1,26 @@
+
 #include <iostream>
 using namespace std;
+void displayworld();
+void setgravitystatus(bool status);
+void timetick(int times);
 
-// Global world (5x5)
 char world[5][5] = {
     {'-', '#', '-', '-', '#'},
     {'#', '-', '-', '#', '-'},
     {'-', '#', '-', '-', '-'},
     {'#', '-', '#', '-', '#'},
     {'#', '-', '-', '-', '-'}};
+bool gravity = false;
+main()
+{
+    displayworld();
+    setgravitystatus(true);
+    timetick(1);
+    displayworld();
+}
 
-bool gravityOn = true;
-void DisplayWorld()
+void displayworld()
 {
     for (int i = 0; i < 5; i++)
     {
@@ -22,57 +32,31 @@ void DisplayWorld()
     }
     cout << endl;
 }
-void SetGravityStatus(bool status)
+void setgravitystatus(bool status)
 {
-    gravityOn = status;
+    gravity = true;
 }
-
-void applyGravityOnce()
+void timetick(int times)
 {
-    for (int j = 0; j < 5; j++) // column-wise
+    if (!gravity)
     {
-        for (int i = 3; i >= 0; i--) // bottom to top
+        return;
+    }
+    else
+    {
+        for (int i = 0; i < times; i++)
         {
-            if (world[i][j] == '#' && world[i + 1][j] == '-')
+            for (int i = 3; i >= 0; i--)
             {
-                // swap
-                world[i + 1][j] = '#';
-                world[i][j] = '-';
+                for (int j = 0; j < 5; j++)
+                {
+                    if (world[i][j] == '#' && world[i + 1][j] == '-')
+                    {
+                        world[i + 1][j] = '#';
+                        world[i][j] = '-';
+                    }
+                }
             }
         }
     }
-}
-
-
-void TimeTick(int times)
-{
-    if (!gravityOn)
-        return;
-
-    for (int t = 0; t < times; t++)
-    {
-        applyGravityOnce();
-    }
-}
-
-
- main()
-{
-    cout << "Initial World:\n";
-    DisplayWorld();
-
-    cout << "Applying 1 Tick:\n";
-    TimeTick(1);
-    DisplayWorld();
-
-    cout << "Applying 2 more Ticks:\n";
-    TimeTick(2);
-    DisplayWorld();
-
-    cout << "Turning Gravity OFF and applying ticks:\n";
-    SetGravityStatus(false);
-    TimeTick(3);
-    DisplayWorld(); // no change expected
-
-    return 0;
 }
