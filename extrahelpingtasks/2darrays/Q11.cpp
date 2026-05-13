3521253b25f9fc2b529d6544c57c52e64e6e4f95
Ammarali-byte <ummaraliummar75@gmail.com>
2026-05-14 01:31:25 +0500

    2darrays

--- extrahelpingtasks/2darrays/Q11.cpp
+++ extrahelpingtasks/2darrays/Q11.cpp
@@ -1,10 +1,22 @@
 #include <iostream>
 using namespace std;
 void inputMatrix(int arr[][100], int m, int n);
-void transposeMatrix(int arr[][100], int m, int n);
+void transposeMatrix(int arr[][100],int transpose[][100] ,int m, int n);
 void printMatrix(int arr[][100], int m, int n);
 main()
 {
+    int m, n;
+    cout << "Enter the number of rows : ";
+    cin >> m;
+    cout << "Enter the number of columns : ";
+    cin >> n;
+    int arr[m][100];
+    int transpose[m][100];
+    inputMatrix(arr , m , n);
+    transposeMatrix(arr,transpose,m ,n);
+    cout<<"after transposed \n";
+    printMatrix(transpose , m , n);
+
 }
 void inputMatrix(int arr[][100], int m, int n)
 {
@@ -17,8 +29,16 @@ void inputMatrix(int arr[][100], int m, int n)
         }
     }
 }
-void transposeMatrix(int arr[][100], int m, int n)
+void transposeMatrix(int arr[][100], int transpose[][100], int m, int n)
 {
+  
+    for(int i = 0 ; i < m ; i++ ){
+        for (int j = 0 ; j < n ; j++)
+        {
+            transpose[i][j] = arr[j][i];
+        }
+    }
+
 }
 void printMatrix(int arr[][100], int m, int n)
 {
