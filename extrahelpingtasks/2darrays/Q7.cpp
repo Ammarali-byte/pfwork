22b52d712ccf88b2bfb8a7d0fce06fe7dcfafacf
Ammarali-byte <ummaraliummar75@gmail.com>
2026-05-14 01:05:24 +0500

    2darrays

--- extrahelpingtasks/2darrays/Q7.cpp
+++ extrahelpingtasks/2darrays/Q7.cpp
@@ -1,19 +1,20 @@
 #include <iostream>
 using namespace std;
-void inputmatrix(int arr[][100], int arr2[][100],int row, int cols);
+void inputmatrix(int arr[][100], int arr2[][100], int row, int cols);
 void outputmatrix(int arr[][100], int rows, int cols);
-int addmatrix(int arr[][100], int arr2[][100], int rows, int cols);
+void addmatrix(int arr[][100], int arr2[][100],int result[][100], int rows, int cols);
 
 main()
 {
-    int rows , cols ;
-    cout<<"Enter the number of rows ";
+    int rows, cols;
+    cout << "Enter the number of rows ";
     cin >> rows;
     cout << "Enter the number of columns ";
     cin >> cols;
-    int arr[rows][100], arr2[rows][100];
-    inputmatrix(arr , arr2 , rows ,cols);
-    int result[rows][cols] = addmatrix(arr, arr2, rows, cols);
+    int arr[100][100], arr2[100][100];
+    inputmatrix(arr, arr2, rows, cols);
+    int result[100][100];
+    addmatrix(arr, arr2,result, rows, cols);
     outputmatrix(result, rows, cols);
 }
 void inputmatrix(int arr[][100], int arr2[][100], int row, int cols)
@@ -37,9 +38,9 @@ void inputmatrix(int arr[][100], int arr2[][100], int row, int cols)
         }
     }
 }
-int  addmatrix(int arr[][100], int arr2[][100], int rows, int cols)
+void addmatrix(int arr[][100], int arr2[][100],int result[][100], int rows, int cols)
 {
-     int result[rows][cols];
+    
     for (int i = 0; i < rows; i++)
     {
         for (int j = 0; j < cols; j++)
@@ -47,7 +48,7 @@ int  addmatrix(int arr[][100], int arr2[][100], int rows, int cols)
             result[i][j] = arr[i][j] + arr2[i][j];
         }
     }
-    return result[rows][cols];
+   
 }
 void outputmatrix(int arr[][100], int rows, int cols)
 {
